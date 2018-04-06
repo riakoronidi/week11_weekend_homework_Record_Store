5272f26d4f881c14269635182d734aa997e5b755
Eleftheria Koronidi <ria@koronidi.com>
2018-04-06 15:20:31 +0100

    complete Part C point 1 - customer can buy and sell records from to Store and TDD done

--- customer.js
+++ customer.js
@@ -1,12 +1,26 @@
 const _ = require("lodash");
 
-var Customer = function(name){
+var Customer = function(name, cash){
   this.name = name;
-  this.records = [];
+  this.cash = cash;
+  this.myRecords = [];
+
+  this.buyFromStore = function(record, store){
+    if(cash > record.price){
+      this.myRecords.push(record);
+      _.pull(store.inventory, record);
+    }
+  };
+
+  this.sellToStore = function(record, store){
+    _.pull(this.myRecords, record);
+    store.inventory.push(record);
+  };
+
 }
 
 Customer.prototype.joinInString = function(){
-  
+
 }
 
 module.exports = Customer;
--- specs/customer_spec.js
+++ specs/customer_spec.js
@@ -1,17 +1,41 @@
 var assert = require('assert');
 var Customer = require('../customer.js');
+var Record = require('../record.js');
+var Store = require('../store.js');
 const _ = require("lodash");
 
 describe('Customer', function() {
   var customer;
+  var store;
+  var record1;
+  var record2;
 
   beforeEach(function(){
-    customer = new Customer("Ria");
+    customer = new Customer("Ria", 30);
+    store = new Store("Ria", "Manchester");
+    record1 = new Record("James", "Laid", "Alternative Rock", 13);
+    record2 = new Record("The Beatles", "A Hard Day's Night", "Rock", 15);
+    record3 = new Record("Scorpions", "Holiday", "Rock", 10);
+    store.addRecord(record1);
+    store.addRecord(record2);
   })
 
 
   it("customer has a name", function() {
     assert.strictEqual(customer.name, "Ria");
+  });
+
+  it("Get Store's Inventory size.", function() {
+    assert.strictEqual(store.inventory.length, 2);
+  });
+
+  it("Customer can buy and sell records", function() {
+    customer.buyFromStore(record1, store);
+    assert.strictEqual(customer.myRecords.length, 1);
+    assert.strictEqual(store.inventory.length, 1);
+
+    customer.sellToStore(record1, store);
+    assert.strictEqual(store.inventory.length, 2);
   });
 
 
